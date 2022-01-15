5ad6ac51842c6d03a3e3bccf41689a293e69fd38
can101 <can896373@gmail.com>
2022-01-15 01:07:53 -0800

    the full files upload process

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    username: "can101",
+    message: "hello"
+  },
+  getters: {
+    wellcome(state) {
+      return `${state.message} ${state.username}`
+    }
   },
   mutations: {
+    setUserName(state, name) {
+      state.username = name;
+    }
   },
   actions: {
+    updateUserName({ commit }, name) {
+      commit('setUserName', name);
+    }
   },
   modules: {
   }
---
+++ src/views/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Counter</h1>
+    <h1>{{ wellcome }}</h1>
+    <input type="text" v-model="name" />
+    <button @click="updtName">Update Name</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from "vuex";
+export default {
+  name: "Counter",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  computed: {
+    ...mapState(["username", "message"]),
+    ...mapGetters(["wellcome"]),
+  },
+  methods: {
+    ...mapActions(["updateUserName"]),
+    updtName() {
+      this.updateUserName(this.name);
+      console.log(this.name);
+    },
+  },
+};
+</script>
+
+<style></style>
